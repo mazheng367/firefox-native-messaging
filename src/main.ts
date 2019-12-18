38c5b0eafb74fc5922aeb5a2d69bf23c4f1047b3
mazheng367@qq.com <mazheng367@qq.com>
2019-12-18 22:08:24 +0800

    b

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-/*jshint esversion:6*/
+/*jshint esversion:9*/
 const div = document.createElement("div");
 div.style.cssText += "position:fixed;right:0;top:10px;border-radius:50%;width:50px;height:50px;background-color:red";
 div.addEventListener("click", async () => {
@@ -11,3 +11,38 @@ div.addEventListener("click", async () => {
     console.log(data);
 });
 document.body.appendChild(div);
+
+
+class LazyMan {
+    private steps: Array<() => void> = [];
+    constructor(name: string) {
+        console.log(`hello ${name}`);
+        setTimeout(() => this.step());
+    }
+
+    eat(food: string) {
+        this.steps.push(() => {
+            console.log(`eat ${food}`);
+            this.step();
+        });
+        return this;
+    }
+
+    sleep(time: number) {
+        this.steps.push(() => {
+            console.log(`sleep ${time}s`);
+            setTimeout(() => {
+                console.log("weak");
+                this.step();
+            }, time * 1000);
+        });
+
+        return this;
+    }
+
+    private step() {
+        this.steps.shift()?.();
+    }
+}
+
+new LazyMan("Maz").eat("apple").sleep(5).eat("orange");
